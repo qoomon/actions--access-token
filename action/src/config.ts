44f985af3174cc0aaaf0ce1860a3c312f11e5678
Johan Lanzrein <jlanzrein@pictet.com>
2025-06-11 15:57:20 +0200

    feat: pass config through action
    * action inputs allow to pass url, roleArn, service and region
    * inputs are parse in the config
    * to be non breaking all default parameters are the same as before

--- action/src/config.ts
+++ action/src/config.ts
@@ -1,4 +1,37 @@
-export const config : Config = {
+import {getYamlInput} from './github-actions-utils.js';
+
+// Type definitions for the configuration
+type ConfigYaml = {
+  api?: {
+    url?: string;
+    auth?: {
+      aws?: {
+        roleArn: string;
+        region: string;
+        service: AllowedService;
+      }
+    }
+  }
+};
+
+interface Config {
+  api: {
+    url: URL
+    auth?: {
+      aws?: {
+        roleArn: string
+        region: string
+        service: AllowedService
+      }
+    }
+  }
+}
+
+// Only allow 'lambda' or 'execute-api' as valid service values.
+const allowedServices = ['lambda', 'execute-api'] as const;
+type AllowedService = typeof allowedServices[number];
+
+export const defaultConfig : Config = {
   api: {
     url: new URL('https://github-actions-access-token.netlify.app'),
     // auth: {
@@ -11,15 +44,19 @@ export const config : Config = {
   },
 };
 
-interface Config {
-  api: {
-    url: URL
-    auth?: {
-      aws?: {
-        roleArn: string
-        region: string
-        service: 'lambda' | 'execute-api'
-      }
-    }
-  }
+export const configYaml: ConfigYaml = getYamlInput('config') || {};
+export const url = new URL(configYaml.api?.url || defaultConfig.api.url.toString());
+export const auth = configYaml.api?.auth || defaultConfig.api.auth;
+
+// Validate service value if present
+const service = auth?.aws?.service;
+if (service && !allowedServices.includes(service)) {
+  throw new Error(`Invalid service: ${service}. Allowed values are: ${allowedServices.join(', ')}`);
 }
+// final configuration object
+export const config: Config = {
+  api: {
+    url,
+    auth: auth,
+  },
+};
