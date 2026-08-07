9c28b0ceb0e2ee7fb7e18b95c07e49adacd614b9
Bengt Brodersen <bengt.brodersen@gmail.com>
2026-08-07 12:36:36 +0200

    Update GitHub Actions token allowed subjects

--- server/deployments/aws/infrastructure/lib/app-stack.ts
+++ server/deployments/aws/infrastructure/lib/app-stack.ts
@@ -9,7 +9,8 @@ import * as path from 'path';
 
 // https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/about-security-hardening-with-openid-connect#example-subject-claims
 const GITHUB_ACTIONS_TOKEN_ALLOWED_SUBJECTS: string[] = [
-  // 'repo:SUSER@123456789/actions--access-token@123456789:ref:refs/heads/main',
+  // 'repo:USER@123456789/*:**', // immutable subject claims
+  // 'repo:USER/*:**', // legacy subject claims
 ]
 
 export class AppStack extends Stack {
